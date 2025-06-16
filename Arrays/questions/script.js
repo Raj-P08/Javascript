5cd7722f951a89128e9f97f80e5fe659666ae931
Raj <rajkumarpatil775694@gmail.com>
2025-06-16 23:18:46 +0530

    Array Questions

--- Arrays/questions/script.js
+++ Arrays/questions/script.js
@@ -1,9 +1,26 @@
-// * 1. For an array of marks of students, find the average marks of the entire class.
+// ! 1. For an array of marks of students, find the average marks of the entire class.
 
-let arr = [85, 90, 78, 92, 88, 76, 95, 89];
-let sum = arr.reduce((acc, curr) => {
-  return acc + curr;
+// let arr = [85, 90, 78, 92, 88, 76, 95, 89];
+// let sum = arr.reduce((acc, curr) => {
+//   return acc + curr;
+// });
+// let average = sum / arr.length;
+// console.log("Sum of marks:", sum);
+// console.log("Average marks of the class:", average);
+
+// ! 2. create an array with the given length(n) and fill with 0.
+
+// let n = 5; // Length of the array
+// let zeroArray = new Array(n).fill(0);
+// console.log("Array filled with 0:", zeroArray);
+
+// ! 3. create an array with the given length(n) and store natural numbers from 1 to n.
+
+let nNatural = 5; // Length of the array
+let naturalArray = new Array(nNatural).fill(0);
+
+naturalArray.forEach((item, index) => {
+  naturalArray[index] = index + 1;
 });
-let average = sum / arr.length;
-console.log("Sum of marks:", sum);
-console.log("Average marks of the class:", average);
+
+console.log(naturalArray);
